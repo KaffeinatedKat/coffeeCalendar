0fb2ba7d999762f09e73f65e27c495e0c2e093ec
John <coffeeboi47@protonmail.com>
2024-07-26 00:42:59 -0600

    fix: first_day_of_week

--- include/calendar_utils.h
+++ include/calendar_utils.h
@@ -8,7 +8,7 @@
 #include <stdbool.h>
 
 uint16_t get_week_number(uint16_t year, uint8_t month, uint8_t day);
-uint8_t day_of_week(uint16_t year, uint8_t month, uint8_t day);
+uint8_t first_day_of_week(uint16_t year, uint8_t month, uint8_t day);
 uint8_t last_day_of_month(uint8_t month, bool leap_year);
 
 bool is_leap_year(uint16_t year);
